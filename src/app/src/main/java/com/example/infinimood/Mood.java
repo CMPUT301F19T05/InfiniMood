aa255450e43f00c34dea8384bf5483e80354e7b3
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2019-10-07 09:52:03 -0600

    feat: draft

---
+++ src/app/src/main/java/com/example/infinimood/Mood.java
@@ -0,0 +1,15 @@
+package com.example.infinimood;
+
+import java.util.Date;
+
+public class Mood {
+
+    private Date date;
+
+    private String emotionalState;
+
+    private String reason;
+
+    private String socialSituation;
+
+}
