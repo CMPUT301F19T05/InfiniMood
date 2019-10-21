3086bba8ec33007bfb15abaa8ccc44d69db67501
Jawad Rizvi <43558589+jnrizvi@users.noreply.github.com>
2019-10-21 15:06:51 -0600

    Jawad (#17)
    * added UI for add/edit mood
    
    * changes to add-edit mood, mood history isn't done yet

---
+++ src/app/src/main/java/com/example/infinimood/AddEditMood.java
@@ -0,0 +1,14 @@
+package com.example.infinimood;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddEditMood extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.addeditmood);
+    }
+}
+
--- src/app/src/main/java/com/example/infinimood/MainActivity.java
+++ src/app/src/main/java/com/example/infinimood/MainActivity.java
@@ -2,14 +2,29 @@ package com.example.infinimood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button test_addEdit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        test_addEdit = findViewById(R.id.test_add_edit);
+        test_addEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AddEditMood.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
+
 
 }
