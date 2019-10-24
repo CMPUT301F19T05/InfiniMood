8d12a36deadfd337b49e702159a1dcc53d20a904
Tyler1232321 <beckingh@ualberta.ca>
2019-10-23 18:54:22 -0600

    Added UI for login and create Account

--- src/app/src/main/java/com/example/infinimood/MainActivity.java
+++ src/app/src/main/java/com/example/infinimood/MainActivity.java
@@ -6,19 +6,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button test_addEdit;
+    TextView signUpTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.user_login);
 
         test_addEdit = findViewById(R.id.test_add_edit);
         test_addEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), AddEditMood.class);
+                startActivity(intent);
+            }
+        });
+
+        signUpTextView = findViewById(R.id.signUpTextView);
+        signUpTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), createAccount.class);
                 startActivity(intent);
             }
         });
---
+++ src/app/src/main/java/com/example/infinimood/createAccount.java
@@ -0,0 +1,14 @@
+package com.example.infinimood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class createAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_account);
+    }
+}
