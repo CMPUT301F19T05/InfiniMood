4e0b8a44c36fe22d520c545e299339a0fe639ce4
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2019-10-27 14:43:46 -0600

    feat: draft

--- src/app/src/main/java/com/example/infinimood/CreateAccountActivity.java
+++ src/app/src/main/java/com/example/infinimood/CreateAccountActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class CreateAccountActivity extends AppCompatActivity {
+public class CreateAccountActivity extends MoodCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
