e13ce2676d925364cc5e93faf2cb4ff6b63ac40d
Jawad Rizvi <jawadrizvi40@gmail.com>
2019-10-28 09:54:45 -0600

    fixed social situation

--- src/app/src/main/java/com/example/infinimood/model/Mood.java
+++ src/app/src/main/java/com/example/infinimood/model/Mood.java
@@ -1,11 +1,6 @@
 package com.example.infinimood.model;
 
-public enum Mood {
-    HAPPY(new MoodEmoticon("happy", "TODO")),
-    ANGRY(new MoodEmoticon("angry", "TODO")),
-    FURIOUS(new MoodEmoticon("furious", "TODO")),
-    LONELY(new MoodEmoticon("lonely", "TODO")),
-    FEARFUL(new MoodEmoticon("fearful", "TODO"));
+public class Mood {
 
     private final MoodEmoticon emoticon;
 
--- src/app/src/main/java/com/example/infinimood/model/SocialSituation.java
+++ src/app/src/main/java/com/example/infinimood/model/SocialSituation.java
@@ -1,10 +1,10 @@
 package com.example.infinimood.model;
 
 public enum SocialSituation {
-    ALONE("alone"),
-    WITH_ONE("with one other person"),
-    WITH_SEVERAL("with two to several people"),
-    WITH_CROWD("with a crowd");
+    ALONE("Alone"),
+    WITH_ONE("With one other person"),
+    WITH_SEVERAL("With two to several people"),
+    WITH_CROWD("With a crowd");
 
     private final String description;
 
