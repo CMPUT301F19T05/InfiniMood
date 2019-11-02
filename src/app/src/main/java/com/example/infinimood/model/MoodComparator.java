b5d523b8c81e3592af6c1f8d0a14da23ec200b8c
Will Fenton <31602935+willfenton@users.noreply.github.com>
2019-11-02 15:18:24 -0600

    Switch to reverse sort order in Mood History (#52)

--- src/app/src/main/java/com/example/infinimood/model/MoodComparator.java
+++ src/app/src/main/java/com/example/infinimood/model/MoodComparator.java
@@ -3,8 +3,16 @@ package com.example.infinimood.model;
 import java.util.Comparator;
 
 public class MoodComparator implements Comparator<Mood> {
+
+    // -1 is reversed, 1 is normal
+    private int reverse = -1;
+
     @Override
     public int compare(Mood m1, Mood m2) {
-        return -Long.compare(m1.getTime(), m2.getTime());
+        return reverse * Long.compare(m1.getTime(), m2.getTime());
+    }
+
+    public void reverse() {
+        reverse = -reverse;
     }
 }
--- src/app/src/main/java/com/example/infinimood/view/MoodHistoryActivity.java
+++ src/app/src/main/java/com/example/infinimood/view/MoodHistoryActivity.java
@@ -1,23 +1,27 @@
 package com.example.infinimood.view;
 
+import android.content.Intent;
 import android.os.Bundle;
-        import android.view.View;
-        import android.widget.ListView;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
-        import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.app.ActionBar;
 
-        import com.example.infinimood.R;
+import com.example.infinimood.R;
         import com.example.infinimood.controller.MoodHistoryAdapter;
         import com.example.infinimood.model.MoodComparator;
-        import com.google.android.material.floatingactionbutton.FloatingActionButton;
-        import com.google.android.material.snackbar.Snackbar;
 
-        import java.util.Collections;
+import java.util.Collections;
 
 public class MoodHistoryActivity extends MoodCompatActivity {
 
     private MoodHistoryAdapter adapter;
     private MoodComparator comparator;
+    private Switch reverseToggle;
 
     // runs when the activity is created
     @Override
@@ -25,8 +29,20 @@ public class MoodHistoryActivity extends MoodCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mood_history);
 
+//        ActionBar actionBar = getSupportActionBar();
+//        actionBar.setDisplayHomeAsUpEnabled(true);
+
         refreshUserMoods();
-        ListView moodListView = findViewById(R.id.history_list_view);
+        ListView moodListView = findViewById(R.id.moodHistoryListView);
+
+        reverseToggle = findViewById(R.id.moodHistorySortOrderButton);
+        reverseToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                comparator.reverse();
+                update();
+            }
+        });
 
         comparator = new MoodComparator();
 
@@ -41,4 +57,9 @@ public class MoodHistoryActivity extends MoodCompatActivity {
         Collections.sort(moods, comparator);
         adapter.notifyDataSetChanged();  // update the ListView
     }
+
+//    public boolean onOptionsItemSelected(MenuItem item){
+//        finish();
+//        return true;
+//    }
 }
