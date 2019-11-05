41328f9f90777c033a08ce24b674d7c653e9b4c4
jonong1 <jzong@ualberta.ca>
2019-11-04 18:22:09 -0700

    added map location

--- src/app/src/main/java/com/example/infinimood/view/UserProfileActivity.java
+++ src/app/src/main/java/com/example/infinimood/view/UserProfileActivity.java
@@ -1,9 +1,11 @@
 package com.example.infinimood.view;
 
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -101,5 +103,10 @@ public class UserProfileActivity extends MoodCompatActivity {
                 }
             }
         });
+    }
+
+    public void onLocationClicked(View view) {
+        final Intent intent = new Intent(this, LocationActivity.class);
+        startActivity(intent);
     }
 }
