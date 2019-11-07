7ffd1476a3011a06ed49d21f42984c76536ffdcf
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2019-11-06 18:58:42 -0700

    feat: decrypt script

--- travis-secrets.sh
+++ travis-secrets.sh
@@ -3,6 +3,9 @@
 set -e
 set -u
 
+GOOGLE_SERVICES=./src/app/google-services.json
+GOOGLE_MAP=./src/app/src/main/res/values/keys.xml
+
 function usage {
     echo 'Usage:'
     echo '    --encrypt  Encrypt secrets from files'
@@ -13,11 +16,21 @@ function usage {
 if [ "$#" -ne '1' ]; then
     usage
 elif [ "$1" == '--encrypt' ]; then
-    travis encrypt-file ./src/app/google-services.json --add
-    travis encrypt-file ./src/app/src/main/res/values/keys.xml --add
+    travis encrypt-file $GOOGLE_SERVICES --add
+    travis encrypt-file $GOOGLE_MAP --add
 elif [ "$1" == '--restore' ]; then
-    echo $GOOGLE_SERVICES_JSON > ./src/app/google-services.json
-    echo $GOOGLE_MAP_API > ./src/app/src/main/res/values/keys.xml
+    openssl aes-256-cbc                \
+        -K $encrypted_0a6446eb3ae3_key \
+        -iv $encrypted_0a6446eb3ae3_iv \
+        -in google-services.json.enc   \
+        -out $GOOGLE_SERVICES          \
+        -d
+    openssl aes-256-cbc                \
+        -K $encrypted_0a6446eb3ae3_key \
+        -iv $encrypted_0a6446eb3ae3_iv \
+        -in keys.xml.enc               \
+        -out $GOOGLE_MAP               \
+        -d
 else
     usage
 fi
