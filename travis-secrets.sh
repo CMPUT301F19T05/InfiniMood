2542b989c1ce95564b78f2062e0fa7129884a274
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2019-11-06 19:07:51 -0700

    feat: update config

--- travis-secrets.sh
+++ travis-secrets.sh
@@ -9,7 +9,6 @@ GOOGLE_MAP=./src/app/src/main/res/values/keys.xml
 function usage {
     echo 'Usage:'
     echo '    --encrypt  Encrypt secrets from files'
-    echo '    --restore  Put secrets in environment variables back to files'
     exit 1
 }
 
@@ -18,19 +17,6 @@ if [ "$#" -ne '1' ]; then
 elif [ "$1" == '--encrypt' ]; then
     travis encrypt-file $GOOGLE_SERVICES --add
     travis encrypt-file $GOOGLE_MAP --add
-elif [ "$1" == '--restore' ]; then
-    openssl aes-256-cbc                \
-        -K $encrypted_0a6446eb3ae3_key \
-        -iv $encrypted_0a6446eb3ae3_iv \
-        -in google-services.json.enc   \
-        -out $GOOGLE_SERVICES          \
-        -d
-    openssl aes-256-cbc                \
-        -K $encrypted_0a6446eb3ae3_key \
-        -iv $encrypted_0a6446eb3ae3_iv \
-        -in keys.xml.enc               \
-        -out $GOOGLE_MAP               \
-        -d
 else
     usage
 fi
