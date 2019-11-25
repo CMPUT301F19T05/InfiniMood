a81cc8f8dc9b2f323cc15a3eba5e35253cc20a8b
Will Fenton <31602935+willfenton@users.noreply.github.com>
2019-11-25 11:04:13 -0700

    Will deleting moods deletes image (#149)
    * Changed date format
    
    * Deleting mood events now deletes the associated image (if any)

--- src/app/src/main/java/com/example/infinimood/fragment/ViewImageFragment.java
+++ src/app/src/main/java/com/example/infinimood/fragment/ViewImageFragment.java
@@ -2,17 +2,11 @@ package com.example.infinimood.fragment;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.graphics.Bitmap;
-import android.location.Location;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,15 +14,8 @@ import androidx.fragment.app.DialogFragment;
 
 import com.example.infinimood.R;
 import com.example.infinimood.controller.BitmapCallback;
-import com.example.infinimood.controller.BooleanCallback;
 import com.example.infinimood.controller.FirebaseController;
 import com.example.infinimood.model.Mood;
-import com.example.infinimood.view.AddEditMoodActivity;
-import com.example.infinimood.view.ViewLocationActivity;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
 
 public class ViewImageFragment extends DialogFragment {
 
@@ -57,7 +44,7 @@ public class ViewImageFragment extends DialogFragment {
         if (mood == null && bitmap != null) {
             imageView.setImageBitmap(bitmap);
         } else if (mood != null && bitmap == null) {
-            firebaseController.getImageFromDB(this.mood, new BitmapCallback() {
+            firebaseController.getMoodImageFromDB(this.mood, new BitmapCallback() {
                 @Override
                 public void onCallback(Bitmap bitmap) {
                     if (bitmap != null) {
