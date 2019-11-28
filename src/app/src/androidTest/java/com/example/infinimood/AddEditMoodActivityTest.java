7d83f34fdcca4d14470bd1a3e5f6962231c80d91
jspenguin2017 <jspenguin2017@users.noreply.github.com>
2019-11-28 13:09:45 -0700

    more tests

--- src/app/src/androidTest/java/com/example/infinimood/AddEditMoodActivityTest.java
+++ src/app/src/androidTest/java/com/example/infinimood/AddEditMoodActivityTest.java
@@ -1,5 +1,9 @@
 package com.example.infinimood;
 
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
@@ -17,6 +21,8 @@ import org.junit.runner.RunWith;
 import java.lang.reflect.InvocationTargetException;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 
 @RunWith(AndroidJUnit4.class)
 public class AddEditMoodActivityTest {
@@ -55,7 +61,35 @@ public class AddEditMoodActivityTest {
         final Mood mood = mockController.addMoodEventToDbCallArgsMood.get(0);
         assertEquals(0, mockController.addImageToDbCallCount, 0);
         assertEquals(1, mockController.addMoodEventToDBCallCount, 0);
+        assertEquals("Happy", mood.getMood());
+        assertEquals("None", mood.getSocialSituation());
         assertEquals("", mood.getReason());
+        assertNull(mood.getLocation());
+        assertFalse(mood.hasImage());
+    }
+
+    @Test
+    public void testAddComplexMood() {
+        solo.assertCurrentActivity("Expected add edit mode screen to show", AddEditMoodActivity.class);
+
+        solo.clickOnView(solo.getView(R.id.addEditMoodSpinner));
+        solo.clickOnText("Angry");
+
+        solo.clickOnView(solo.getView(R.id.addEditSocialSituationSpinner));
+        solo.clickOnText("With a crowd");
+
+        solo.enterText((EditText) solo.getView(R.id.addEditReasonEditText), "REEEE");
+
+        solo.clickOnButton(solo.getString(R.string.submit));
+        solo.waitForText(solo.getString(R.string.add_mood_successfully_saved));
+        final Mood mood = mockController.addMoodEventToDbCallArgsMood.get(0);
+        assertEquals(0, mockController.addImageToDbCallCount, 0);
+        assertEquals(1, mockController.addMoodEventToDBCallCount, 0);
+        assertEquals("Angry", mood.getMood());
+        assertEquals("With a crowd", mood.getSocialSituation());
+        assertEquals("REEEE", mood.getReason());
+        assertNull(mood.getLocation());
+        assertFalse(mood.hasImage());
     }
 
 }
